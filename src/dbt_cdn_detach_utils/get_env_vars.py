fc60d050d6dfb2575315f4e02a331f413b3b5d1a
Kier Davis <kier.davis@businessandtrade.gov.uk>
2026-03-20 13:16:14 +0000

    fix

--- src/dbt_cdn_detach_utils/get_env_vars.py
+++ src/dbt_cdn_detach_utils/get_env_vars.py
@@ -7,8 +7,19 @@ def main():
     parser.add_argument("env")
     args = parser.parse_args()
 
+    with open("platform-config.yml") as f:
+        platform_config = yaml.safe_load(f)
+
+    env_name = args.env
+    env_config = {
+        **(platform_config["environments"].get("*") or {}),
+        **(platform_config["environments"].get(env_name) or {}),
+    }
+    account_id = env_config["accounts"]["deploy"]["id"]
+    profile_name = get_profile_name_from_account_id(account_id)
+
     print(f"export ENVIRONMENT={shlex.quote(args.env)}")
-    print("export AWS_PROFILE=$(get-aws-profile $ENVIRONMENT)")
+    print(f"export AWS_PROFILE={shlex.quote(profile_name)}")
     print("export AWS_REGION=eu-west-2")
     print("export AWS_DEFAULT_REGION=eu-west-2")
     print("export PLATFORM_HELPER_VERSION_OVERRIDE=$(platform-helper version get-platform-helper-for-project)")
